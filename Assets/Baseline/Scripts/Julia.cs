c2c0a9f8f89908737bc4d61dd1b58b16c7958641
Adrià Torralba <adry.torralba@gmail.com>
2020-05-11 01:22:15 +0200

    [UI] More colormaps added.

--- Assets/Baseline/Scripts/Julia.cs
+++ Assets/Baseline/Scripts/Julia.cs
@@ -5,14 +5,21 @@ using System.Numerics;
 using System;
 
 
-
 public class Julia : Fractal
 {
+
+    private Color[] colors;
+    void Start(){
+        colors = new Color[256];
+        for(int i = 0; i < 255; i++){
+            colors[i] = Color.HSVToRGB((float)i/256.0f, 1.0f, 1.0f);
+        }
+        colors[255] = Color.black;
+    }
     public double reZ;
     public double imZ;
     public override void StartDraw(){
         throw new Exception("(!) You should call StartDraw(double reZ, double imZ) instead.");
-        StartDraw(reZ, imZ);
     }
 
     public void StartDraw(double rez, double imz){
@@ -84,39 +91,18 @@ public class Julia : Fractal
         for (x = 0; x < rp.pwidth; x++){
             for (y = 0; y < rp.pheight; y++){
                 Color value;
-                rp.viewPortX = rp.xmin + ((double) x / rp.pwidth) * rp.viewPortWidth + rp.panX;
-                rp.viewPortY = rp.ymin + ((double) y / rp.pheight) * rp.viewPortHeight + rp.panY;
-                Complex z = new Complex(rp.viewPortX, rp.viewPortY);
-                Complex c = new Complex(reZ, imZ);
-                i = 0;
-                while (Complex.Abs(z) < 2 && i < fp.maxIters){
-                    z = Complex.Pow(z, 2) + c;
-                    i++;
-                }
-
-                if (i == fp.maxIters){
-                    value = new Color(0.0f, 0.0f, 0.0f, 1.0f);
-                }else{
-                    switch (fp.colorMap){
-                        case "Colormap 1":
-                            value = new Color(Mathf.Sin((float)i/4) / 4.0f + 0.75f, Mathf.Sin((float)i/5) / 4.0f + 0.75f, Mathf.Sin((float)i/7) / 4.0f + 0.75f, 1.0f);
-                            break;
-                        case "Black and White":
-                            value = new Color(1.0f, 1.0f, 1.0f, 1.0f); 
-                            break;
-                        default:
-                            value = new Color(Mathf.Sin((float)i/4) / 4.0f + 0.75f, Mathf.Sin((float)i/5) / 4.0f + 0.75f, Mathf.Sin((float)i/7) / 4.0f + 0.75f, 1.0f);
-                            break;
-                    }
-                }
+                i = ComputeConvergence(x, y, reZ, imZ);
+                value = PickColor(i);
                 rp.count++;
                 rp.tex2D.SetPixel((int) x, (int) y, value);
-            }
-            
-            if (x % 188 == 0){
-                rp.tex2D.Apply();
-                rp.fractalImage.sprite = Sprite.Create(rp.tex2D, new Rect(0, 0, rp.tex2D.width, rp.tex2D.height), new UnityEngine.Vector2(0.5f, 0.5f)); 
-                yield return new WaitForEndOfFrame();
+                // For display purposes
+                
+                }
+                if (x % 10 == 0){
+                    // rp.tex2D.Apply();
+                    // rp.fractalImage.sprite = Sprite.Create(rp.tex2D, new Rect(0, 0, rp.tex2D.width, rp.tex2D.height), new UnityEngine.Vector2(0.5f, 0.5f)); 
+                    // yield return new WaitForEndOfFrame();
+                    yield return new WaitForSeconds(0.0001f);
             }
         }
         rp.tex2D.Apply();
@@ -125,6 +111,55 @@ public class Julia : Fractal
         rp.finished = true;
         watch.Stop();
         LogsController.UpdateLogs(new string[] {"Julia drawing corroutine finished successfully in " + watch.ElapsedMilliseconds/1000.0+  "s!"}, "#75FF00");
+    }
 
+    private int ComputeConvergence(int x, int y, double reZ, double imZ){
+        rp.viewPortX = rp.xmin + ((double) x / rp.pwidth) * rp.viewPortWidth + rp.panX;
+        rp.viewPortY = rp.ymin + ((double) y / rp.pheight) * rp.viewPortHeight + rp.panY;
+        Complex z = new Complex(rp.viewPortX, rp.viewPortY);
+        Complex c = new Complex(reZ, imZ);
+        int i = 0;
+        while (Complex.Abs(z) < 2 && i < fp.maxIters){
+            z = Complex.Pow(z, 2) + c;
+            i++;
+        }
+        return i;
+    }
+
+    private Color PickColor(int i){
+        switch (fp.colorMap){
+            case "Colormap 1 (Sin)":
+                if (i == fp.maxIters){
+                    return new Color(0.0f, 0.0f, 0.0f, 1.0f);
+                }else{
+                    return new Color(Mathf.Sin((float)i/4) / 4.0f + 0.75f, Mathf.Sin((float)i/5) / 4.0f + 0.75f, Mathf.Sin((float)i/7) / 4.0f + 0.75f, 1.0f);
+                }
+            case "Colormap 2 (Cos)":
+                if (i == fp.maxIters){
+                    return new Color(0.0f, 0.0f, 0.0f, 1.0f);
+                }else{
+                    return new Color(Mathf.Cos((float)i/4) / 4.0f + 0.75f, Mathf.Cos((float)i/5) / 4.0f + 0.75f, Mathf.Cos((float)i/7) / 4.0f + 0.75f, 1.0f);
+                }
+            case "Colormap 3 (Tan)":
+                if (i == fp.maxIters){
+                    return new Color(0.0f, 0.0f, 0.0f, 1.0f);
+                }else{
+                    return new Color(Mathf.Tan((float)i/4) / 4.0f + 0.75f, Mathf.Tan((float)i/5) / 4.0f + 0.75f, Mathf.Tan((float)i/7) / 4.0f + 0.75f, 1.0f);
+                }
+            case "Gradient":
+                return colors[(int)(255 * (double) i / fp.maxIters)];
+            case "Black and White":
+                if (i == fp.maxIters){
+                    return new Color(0.0f, 0.0f, 0.0f, 1.0f); 
+                }else{
+                    return new Color(1.0f, 1.0f, 1.0f, 1.0f); 
+                }
+            default:
+                if (i == fp.maxIters){
+                    return new Color(0.0f, 0.0f, 0.0f, 1.0f);
+                }else{
+                    return new Color(Mathf.Sin((float)i/4) / 4.0f + 0.75f, Mathf.Sin((float)i/5) / 4.0f + 0.75f, Mathf.Sin((float)i/7) / 4.0f + 0.75f, 1.0f);
+                }
+        }
     }
 }
--- Assets/Baseline/Scripts/Mandelbrot.cs
+++ Assets/Baseline/Scripts/Mandelbrot.cs
@@ -6,6 +6,15 @@ using System.Numerics;
 
 public class Mandelbrot : Fractal
 {
+    private Color[] colors;
+    void Start(){
+        colors = new Color[256];
+        for(int i = 0; i < 255; i++){
+            colors[i] = Color.HSVToRGB((float)i/256.0f, 1.0f, 1.0f);
+        }
+        colors[255] = Color.black;
+    }
+
     public override void StartDraw(){
         try{
             StopCoroutine(rp.drawingThread);
@@ -49,39 +58,18 @@ public class Mandelbrot : Fractal
         for (x = 0; x < rp.pwidth; x++){
             for (y = 0; y < rp.pheight; y++){
                 Color value;
-                rp.viewPortX = rp.xmin + ((double) x / rp.pwidth) * rp.viewPortWidth + rp.panX;
-                rp.viewPortY = rp.ymin + ((double) y / rp.pheight) * rp.viewPortHeight + rp.panY;
-                Complex z = new Complex(0.0f, 0.0f);
-                Complex c = new Complex(rp.viewPortX, rp.viewPortY);
-                i = 0;
-                while (Complex.Abs(z) < 2 && i < fp.maxIters){
-                    z = Complex.Pow(z, 2) + c;
-                    i++;
-                }
-
-                if (i == fp.maxIters){
-                    value = new Color(0.0f, 0.0f, 0.0f, 1.0f);
-                }else{
-                    switch (fp.colorMap){
-                        case "Colormap 1":
-                            value = new Color(Mathf.Sin((float)i/4) / 4.0f + 0.75f, Mathf.Sin((float)i/5) / 4.0f + 0.75f, Mathf.Sin((float)i/7) / 4.0f + 0.75f, 1.0f);
-                            break;
-                        case "Black and White":
-                            value = new Color(1.0f, 1.0f, 1.0f, 1.0f); 
-                            break;
-                        default:
-                            value = new Color(Mathf.Sin((float)i/4) / 4.0f + 0.75f, Mathf.Sin((float)i/5) / 4.0f + 0.75f, Mathf.Sin((float)i/7) / 4.0f + 0.75f, 1.0f);
-                            break;
-                    }
-                }
+                i = ComputeConvergence(x, y);
+                value = PickColor(i);
                 rp.count++;
                 rp.tex2D.SetPixel((int) x, (int) y, value);
+                // For display purposes
+                
             }
-            
-            if (x % 188 == 0){
-                rp.tex2D.Apply();
-                rp.fractalImage.sprite = Sprite.Create(rp.tex2D, new Rect(0, 0, rp.tex2D.width, rp.tex2D.height), new UnityEngine.Vector2(0.5f, 0.5f)); 
-                yield return new WaitForEndOfFrame();
+             if (x % 10 == 0){
+            //     rp.tex2D.Apply();
+            //     rp.fractalImage.sprite = Sprite.Create(rp.tex2D, new Rect(0, 0, rp.tex2D.width, rp.tex2D.height), new UnityEngine.Vector2(0.5f, 0.5f)); 
+            //     yield return new WaitForEndOfFrame();
+                yield return new WaitForSeconds(0.0001f);
             }
         }
         rp.tex2D.Apply();
@@ -90,6 +78,55 @@ public class Mandelbrot : Fractal
         rp.finished = true;
         watch.Stop();
         LogsController.UpdateLogs(new string[] {"Mandelbrot drawing corroutine finished successfully in " + watch.ElapsedMilliseconds/1000.0+  "s!"}, "#75FF00");
+    }
 
+    private int ComputeConvergence(int x, int y){
+        rp.viewPortX = rp.xmin + ((double) x / rp.pwidth) * rp.viewPortWidth + rp.panX;
+        rp.viewPortY = rp.ymin + ((double) y / rp.pheight) * rp.viewPortHeight + rp.panY;
+        Complex z = new Complex(0.0f, 0.0f);
+        Complex c = new Complex(rp.viewPortX, rp.viewPortY);
+        int i = 0;
+        while (Complex.Abs(z) < 2 && i < fp.maxIters){
+            z = Complex.Pow(z, 2) + c;
+            i++;
+        }
+        return i;
+    }
+
+    private Color PickColor(int i){
+        switch (fp.colorMap){
+            case "Colormap 1 (Sin)":
+                if (i == fp.maxIters){
+                    return new Color(0.0f, 0.0f, 0.0f, 1.0f);
+                }else{
+                    return new Color(Mathf.Sin((float)i/4) / 4.0f + 0.75f, Mathf.Sin((float)i/5) / 4.0f + 0.75f, Mathf.Sin((float)i/7) / 4.0f + 0.75f, 1.0f);
+                }
+            case "Colormap 2 (Cos)":
+                if (i == fp.maxIters){
+                    return new Color(0.0f, 0.0f, 0.0f, 1.0f);
+                }else{
+                    return new Color(Mathf.Cos((float)i/4) / 4.0f + 0.75f, Mathf.Cos((float)i/5) / 4.0f + 0.75f, Mathf.Cos((float)i/7) / 4.0f + 0.75f, 1.0f);
+                }
+            case "Colormap 3 (Tan)":
+                if (i == fp.maxIters){
+                    return new Color(0.0f, 0.0f, 0.0f, 1.0f);
+                }else{
+                    return new Color(Mathf.Tan((float)i/4) / 4.0f + 0.75f, Mathf.Tan((float)i/5) / 4.0f + 0.75f, Mathf.Tan((float)i/7) / 4.0f + 0.75f, 1.0f);
+                }
+            case "Gradient":
+                return colors[(int)(255 * (double) i / fp.maxIters)];
+            case "Black and White":
+                if (i == fp.maxIters){
+                    return new Color(0.0f, 0.0f, 0.0f, 1.0f); 
+                }else{
+                    return new Color(1.0f, 1.0f, 1.0f, 1.0f); 
+                }
+            default:
+                if (i == fp.maxIters){
+                    return new Color(0.0f, 0.0f, 0.0f, 1.0f);
+                }else{
+                    return new Color(Mathf.Sin((float)i/4) / 4.0f + 0.75f, Mathf.Sin((float)i/5) / 4.0f + 0.75f, Mathf.Sin((float)i/7) / 4.0f + 0.75f, 1.0f);
+                }
+        }
     }
 }
